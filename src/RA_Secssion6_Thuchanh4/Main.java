9193cad512a5404d262127acc6a48c75ba1fa89d
tanquynh <tanquynh1605@gmail.com>
2023-09-22 08:03:35 +0700

    firt commit

--- src/RA_Secssion6_Thuchanh4/Main.java
+++ src/RA_Secssion6_Thuchanh4/Main.java
@@ -3,29 +3,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // Khởi tạo đối tượng Scanner để đọc dữ liệu từ bàn phím.
+        Scanner scanner = new Scanner(System.in);
 
-        Calculator calculator = new Calculator(); // Tạo một đối tượng máy tính từ lớp Calculator bạn đã định nghĩa.
+        Calculator calculator = new Calculator();
 
-        System.out.print("Nhập a = "); // Hiển thị thông báo để nhập giá trị cho a.
-        double a = scanner.nextDouble(); // Đọc giá trị của a từ bàn phím.
+        System.out.print("Nhập a = ");
+        double a = scanner.nextDouble();
 
-        System.out.print("Nhập b = "); // Hiển thị thông báo để nhập giá trị cho b.
-        double b = scanner.nextDouble(); // Đọc giá trị của b từ bàn phím.
+        System.out.print("Nhập b = ");
+        double b = scanner.nextDouble();
 
-        calculator.setA(a); // Đặt giá trị của a vào đối tượng máy tính.
-        calculator.setB(b); // Đặt giá trị của b vào đối tượng máy tính.
+        calculator.setA(a);
+        calculator.setB(b);
 
-        System.out.println("Tổng 2 số là: " + calculator.add()); // In ra tổng của a và b.
-        System.out.println("Hiệu 2 số là: " + calculator.sub()); // In ra hiệu của a và b.
-        System.out.println("Tích 2 số là: " + calculator.multi()); // In ra tích của a và b.
+        System.out.println("Tổng 2 số là: " + calculator.add());
+        System.out.println("Hiệu 2 số là: " + calculator.sub());
+        System.out.println("Tích 2 số là: " + calculator.multi());
 
         if (b != 0) {
-            System.out.println("Thương 2 số là: " + calculator.div()); // Kiểm tra xem b có khác 0 không trước khi thực hiện phép chia và in ra thương của a và b.
+            System.out.println("Thương 2 số là: " + calculator.div());
         } else {
-            System.out.println("Không thể chia cho 0."); // Nếu b = 0, in ra thông báo không thể chia cho 0.
+            System.out.println("Không thể chia cho 0.");
         }
 
-        scanner.close(); // Đóng đối tượng Scanner khi đã sử dụng xong để giải phóng tài nguyên hệ thống.
+        scanner.close();
     }
 }
